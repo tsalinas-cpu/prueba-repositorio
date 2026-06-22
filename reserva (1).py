02786a52feb4ac089968e08ae21f10eb122ce106
edmumartinez-cmd <edmu.martinez@duocuc.cl>
2026-06-21 20:25:37 -0400

    Add validation functions for positive integers and floats

--- reserva (1).py
+++ reserva (1).py
@@ -5,4 +5,32 @@ def validar_texto(mensaje):
         texto = input(mensaje).strip()
         if texto != "":
             return texto
-        print("Error: el campo no puede estar vacío.")
+        print("Error, el campo no puede estar vacío.")
+        def validar_entero_positivo(mensaje):
+            while true:
+                try:
+                    numero = int(input(mensaje))
+                    if numero >0:
+                        return numero
+                    else:
+                        print("Error,debe ingresar un numero mayor a 0")
+                except ValueError:
+                    print("Error,debe ingresar un n umero valido")
+
+def validar_float_positivo (mensaje):
+    while true:
+        try:
+            numero = float(input(mensaje))
+            if numero >0:
+                return numero
+            else:
+                print("Error,numero debe ser mayor a 0")
+        except ValueError:
+            print("Error,debe ingresar un valor valido")
+
+#Funciones de calculo
+
+
+
+
+        
